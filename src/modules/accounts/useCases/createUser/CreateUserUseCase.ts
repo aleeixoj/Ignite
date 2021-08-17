8bcfb21520cbce20a28a5d6b3c37ed4f403acbe0
aleeixoj <aleixoo.junior@telefonica.com>
2021-08-17 14:54:13 -0300

    Users

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,36 @@
+import { hash } from 'bcrypt';
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute({
+    driver_license,
+    email,
+    name,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const passwordHash = await hash(password, 8);
+
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    await this.usersRepository.create({
+      driver_license,
+      email,
+      name,
+      password: passwordHash,
+    });
+  }
+}
+
+export { CreateUserUseCase };
